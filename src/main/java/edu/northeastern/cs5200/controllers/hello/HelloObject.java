69b94a2db80ea82ca8d9b589d329418b3d9458c0
tianqingche <yu.ya@husky.neu.edu>
2018-01-30 18:41:10 -0500

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,9 +1,24 @@
 package edu.northeastern.cs5200.controllers.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @RequestMapping
+@Entity(name="hello")
 public class HelloObject {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	public int getId() {
+		  return id;
+	}
+	 public void setId(int id) {
+	  this.id = id;
+	}
+	 
     private String message;
     public String getMessage() {
         return message;
@@ -11,6 +26,7 @@ public class HelloObject {
     public void setMessage(String message) {
         this.message = message;
     }
+    
     public HelloObject(String message) {
         this.message = message;
     }
